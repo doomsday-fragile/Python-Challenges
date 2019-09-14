b84603c74b2b90db1a6f1e21e2f4af45a98dc8ca
Gaurav Malik <37213011+doomsday-fragile@users.noreply.github.com>
2019-09-14 17:36:31 +0530

    Add files via upload

--- First and Last Indices of an Element in a Sorted Array/First Solution.py
+++ First and Last Indices of an Element in a Sorted Array/First Solution.py
@@ -22,19 +22,19 @@ class Solution:
 arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8] 
 x = 2
 print(Solution().getRange(arr, x))
-# [1, 4]
+#Desired Output [1, 4]
 
 arr = [1,3,3,5,7,8,9,9,9,15]
 x = 9
 print(Solution().getRange(arr, x))
-#[6,8]
+#Desired Output [6,8]
 
 arr = [100, 150, 150, 153]
 x = 150
 print(Solution().getRange(arr, x))
-#[1,2]
+#Desired Output [1,2]
 
 arr = [1,2,3,4,5,6,10]
 x = 9
 print(Solution().getRange(arr, x))
-#[-1,-1]
+#Desired Output [-1,-1]
